fa65eb2c3cc7230b9aba51a22755cfe793f65aba
Ryan Wise <ryan@ryanwise.me>
2020-03-12 23:48:32 -0400

    update

--- misc/decompress.go
+++ misc/decompress.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"strconv"
+	"strings"
 )
 
 /*
@@ -23,10 +25,18 @@ func main() {
 		panic("fucked up on the file")
 	}
 
+	d = StringToByteArray("73 82 79 78 21 3 32 11 3 40 0 3 41 20 3 44 10 4 46 3 13 3 49 7 3 50 4 9 52 1 2 5 6 17 18 19 4 54 8 15 5 55 9 12 14 3 57 16")
+
 	// First 4 are garbage and just say IRON
 	d = d[4:]
 
+	length := int(d[5])
 	result := make(map[int]rune)
+
+	fmt.Println(length)
+	for i := 0; i < length; i++ {
+		result[i] = rune(32)
+	}
 
 	for bytePos := 1; bytePos < len(d); {
 		offset := int(d[bytePos])
@@ -34,7 +44,6 @@ func main() {
 		positions := d[bytePos+2 : bytePos+offset]
 
 		AddCharToMap(&result, char, positions)
-
 		fmt.Printf("Character: %s Positions -> ", string(char))
 		PrintHex(positions)
 
@@ -44,8 +53,21 @@ func main() {
 	PrintMap(&result)
 }
 
+func StringToByteArray(s string) []byte {
+	a := strings.Fields(s)
+	fmt.Printf("%v", a)
+	bytes := make([]byte, len(a))
+	for i, b := range a {
+		y, _ := strconv.Atoi(b)
+		bytes[i] = byte(y)
+	}
+	fmt.Printf("%v", bytes)
+	return bytes
+}
+
 func PrintMap(m *map[int]rune) {
 	l := len(*m)
+	fmt.Println(l)
 	for i := 0; i < l; i++ {
 		fmt.Printf("%c", (*m)[i])
 	}
